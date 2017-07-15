4741ebd6e36fc0d8d5c546ed069ed9127324d86f
Nurul Amin <amin.ict@gmail.com>
2017-07-15 13:02:23 +0600

    List class define

---
+++ includes/admin/class-list.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Handle all function of list related.  
+ */
+class managx_list {
+
+    function __construct() {
+        
+    }
+
+    function get_lists( $project_id ) {
+        
+    }
+
+    function get_list( $list_id ) {
+        
+    }
+
+    function create_list( $project_id ) {
+        
+    }
+
+    function update_list( $list_id, $data ) {
+        
+    }
+
+    function delete_list( $list_id ) {
+        
+    }
+
+}
