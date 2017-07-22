256d0cb519b18f624ba0cd96f4898c878702b166
ashique <ashique12009@gmail.com>
2017-07-22 17:12:26 +0600

    Create project container UI changed first attemp

--- templates/admin/projects/template-project-form.php
+++ templates/admin/projects/template-project-form.php
@@ -1,35 +1,26 @@
 <script type="text/x-template" id="tmpl-managx-project-form">
-    <form id="create-project-form" > 
-    <div> 
-        <h1>Project Form</h1> 
+    <form id="create-project-form">
+        <div id="create-project-form-id">
+            <h2 class="mt0">Project Form</h2>
 
-        <div class="form-group"> 
-            <label>
-                Project Name
-            </label>
-            <input class="form-control" value="" v-model="form.name" name="name" />
-        </div>
-
-        <div class="form-group"> 
-            <label>
-            Project Details
-            </label>
-            <textarea class="form-control" name="description"  v-model="form.description"></textarea>
-        </div> 
-
-        <div class="form-group"> 
-            <label>
-            Assign user 
-            </label> 
-            <div>
-            Assign User List 
+            <div class="form-inner-row">
+                <label class="form-inner-lbl">Project Name</label>
+                <input type="text" class="" value="" v-model="form.name" name="name" />
             </div>
-    
-            <div class="form-group"> 
-                <input type="button" value="Submit"  name="create_project" @click="create_project" />  
-            </div> 
-        </div>  
-    </div>
+
+            <div class="form-inner-row">
+                <label class="form-inner-lbl">Project Details</label>
+                <textarea class="" name="description"  v-model="form.description"></textarea>
+            </div>
+
+            <div class="form-inner-row">
+                <label class="">Assign user</label>
+                <div>Assign User List</div>
+                <div class="">
+                    <input type="button" class="btn btn-primary mt10" value="Submit" name="create_project" @click="create_project" />
+                </div>
+            </div>
+        </div>
     </form>
 </script>
 <?php
