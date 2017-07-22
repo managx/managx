242b921a54e93298e292eab2e906cba38296cc12
ashique <ashique12009@gmail.com>
2017-07-22 17:54:30 +0600

    Project creation form UI done first attemp

--- templates/admin/projects/template-project-form.php
+++ templates/admin/projects/template-project-form.php
@@ -1,21 +1,21 @@
 <script type="text/x-template" id="tmpl-managx-project-form">
     <form id="create-project-form">
-        <div>
+        <div id="create-project-form-id">
             <h2 class="mt0">Project Form</h2>
 
             <div class="form-inner-row">
-                <label>Project Name</label>
+                <label class="form-inner-lbl">Project Name</label>
                 <input type="text" class="" value="" v-model="form.name" name="name" />
             </div>
 
             <div class="form-inner-row">
-                <label>Project Details</label>
+                <label class="form-inner-lbl">Project Details</label>
                 <textarea class="" name="description" v-model="form.description"></textarea>
             </div>
 
             <div class="form-inner-row">
-                <label>Assign User</label>
-                <div>
+                <label class="form-inner-lbl">Assign User</label>
+                <div class="form-inner-right-side-element">
                     <multiselect v-model="user" :options="['Select option', 'options', 'selected', 'mulitple', 'label', 'searchable', 'clearOnSelect', 'hideSelected', 'maxHeight', 'allowEmpty', 'showLabels', 'onChange', 'touched']" :searchable="false" :close-on-select="true" :show-labels="false" placeholder="Select User"></multiselect>
                 </div>
 
